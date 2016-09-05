7aec6543ea136d7697ac92432f4c35f0496a4b61
Christopher Arndt <chris@chrisarndt.de>
2016-09-06 01:24:01 +0200

    Add example using encoder library
    Signed-off-by: Christopher Arndt <chris@chrisarndt.de>

---
+++ examples/enc_oscclient.py
@@ -0,0 +1,34 @@
+from time import sleep_ms
+from encoder import Encoder
+from uosc.client import Client
+
+
+UPDATE_DELAY = const(50)
+OSC_SERVER = '192.168.4.2'
+OSC_PORT = const(9001)
+OSC_TOPIC = '/midi'
+MIDI_CC = const(7)
+
+
+def main():
+    enc = Encoder(pin_clk=12, pin_dt=14, clicks=4, accel=5, max_val=127)
+    osc = Client(OSC_SERVER, OSC_PORT)
+
+    oldval = 0
+    try:
+        while True:
+            val = enc.value
+            if oldval != val:
+                oldval = val
+
+                osc.send(OSC_TOPIC, ('m', (0xB0, MIDI_CC, val, 0)))
+
+            enc.cur_accel = max(0, enc.cur_accel - enc.accel)
+            sleep_ms(UPDATE_DELAY)
+    except Exception as exc:
+        enc.close()
+        print(exc)
+
+
+if __name__ == '__main__':
+    main()
