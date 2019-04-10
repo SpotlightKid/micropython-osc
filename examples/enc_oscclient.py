10337b7af51aef00a7816ee1f743441a771e9449
Christopher Arndt <chris@chrisarndt.de>
2019-04-10 13:27:20 +0200

    Add switch handling to encoder example
    Signed-off-by: Christopher Arndt <chris@chrisarndt.de>

--- examples/enc_oscclient.py
+++ examples/enc_oscclient.py
@@ -1,29 +1,39 @@
+from machine import Pin
 from time import sleep_ms
 from encoder import Encoder
 from uosc.client import Client
 
 
 UPDATE_DELAY = const(50)
-OSC_SERVER = '192.168.42.151'
+OSC_SERVER = '192.168.42.1'
 OSC_PORT = const(12101)
 OSC_TOPIC = '/midi'
-MIDI_CC = const(7)
+MIDI_CC_ENC = const(7)
+MIDI_CC_SW = const(64)
+PIN_CLK = const(4)
+PIN_DT = const(0)
+PIN_SW = const(2)
 
 
 def main():
-    enc = Encoder(pin_clk=12, pin_dt=14, clicks=4, accel=5, max_val=127)
+    enc = Encoder(pin_clk=PIN_CLK, pin_dt=PIN_DT, clicks=4, accel=5, max_val=127)
     osc = Client(OSC_SERVER, OSC_PORT)
+    sw = Pin(PIN_SW, Pin.IN, None)
 
     oldval = 0
+    oldsw = 1
     try:
         while True:
-            val = enc.value
-            if oldval != val:
-                oldval = val
-
-                osc.send(OSC_TOPIC, ('m', (0, 0xB0, MIDI_CC, val)))
+            if enc.value != oldval:
+                osc.send(OSC_TOPIC, ('m', (0, 0xB0, MIDI_CC_ENC, enc.value)))
+                oldval = enc.value
 
             enc.cur_accel = max(0, enc.cur_accel - enc.accel)
+
+            if sw() != oldsw:
+                osc.send(OSC_TOPIC, ('m', (0, 0xB0, MIDI_CC_SW, 0 if sw() else 127)))
+                oldsw = sw()
+
             sleep_ms(UPDATE_DELAY)
     except Exception as exc:
         enc.close()
